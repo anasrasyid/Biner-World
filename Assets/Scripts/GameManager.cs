0aad582dbef5ba25237fd87d7b2d4dc4d663180f
anasrasyid <anasrasyid333@gmail.com>
2019-11-20 19:34:08 +0700

    Essential Player Settings
    Player Movement, Player  Jump, and Player UI Button

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Text text;
+    private static GameObject instance;
+    private float time;
+    public Player player;
+
+    private void Start()
+    {
+        if(instance == null)
+        {
+            instance = gameObject;
+            DontDestroyOnLoad(instance);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        time = 0;
+        player = GameObject.Find("Player").GetComponent<Player>();
+    }
+    private void Update()
+    {
+        VisualTime(Mathf.RoundToInt(time));
+        time += Time.deltaTime;
+        Death();
+    }
+
+    private void VisualTime(int time)
+    {
+        if(time > 60)
+        {
+            text.text = (time / 60).ToString() + ":" + (time%60).ToString();
+        }
+        else
+        {
+            text.text = time.ToString();
+        }
+    }
+
+    private void Death()
+    {
+        if(player == null)
+        {
+            text.text = "\n Game Over";
+        }
+    }
+    
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    #region Player Controller
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float jumpPower;
+    #endregion
+    private float horizontal;
+    private bool canMove;
+
+    private Rigidbody2D rigidbody2;
+    private SpriteRenderer spriteRenderer;
+    private Collision2D ground;
+    private Touch theTouch;
+    private Vector2 start, end;
+    private Animator anim;
+
+    void Start()
+    {
+        rigidbody2 = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        ground = null;
+        canMove = false;
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        //Jump();
+        //float horizontal = Input.GetAxisRaw("Horizontal");
+        Movement();
+    }
+
+    public void Directional(float direct)
+    {
+        canMove = true;
+        horizontal = direct;
+        anim.SetBool("canMove", canMove);
+    }
+
+    public void StopMovement()
+    {
+        canMove = false;
+        horizontal = 0;
+        rigidbody2.velocity = new Vector2(transform.right.x * horizontal * speed, rigidbody2.velocity.y);
+        anim.SetBool("canMove", canMove);
+    }
+
+    public void Movement()
+    {
+        if (!canMove) return;
+        if (horizontal > 0)
+        {
+            spriteRenderer.flipX = false;
+        }else if(horizontal < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        rigidbody2.velocity = new Vector2(transform.right.x * horizontal * speed, rigidbody2.velocity.y);
+    }
+
+    public void Jump()
+    {
+        if (ground == null)
+            return;
+        anim.SetTrigger("isJump");
+        rigidbody2.velocity = Vector2.up * jumpPower;
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            ground = collision;
+        }
+    }
+
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            ground = null;
+        }
+    }
+}
